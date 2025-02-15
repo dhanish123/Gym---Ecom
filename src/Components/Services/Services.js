17db08ff0818999348fe6a2d099ec2b023509c5c
Dhanish <dhanishmohammed814@gmail.com>
2025-02-15 16:15:02 +0530

    mobile issue api fixed

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -7,9 +7,13 @@ const Services = () => {
   const [datas, setDatas] = useState([]); // Ensure datas is always an array
   const [searchQuery, setSearchQuery] = useState("");
 
+  const API_URL = "https://gym-ecom.vercel.app/"; // Replace with actual API URL
+
   const getData = async () => {
     try {
-      const resp = await fetch("http://localhost:5000/data");
+      // const resp = await fetch("http://localhost:5000/data");
+      const resp = await fetch(`${API_URL}/data`);
+      
       if (!resp.ok) throw new Error("Failed to fetch data");
 
       const json = await resp.json();
